827c301f025292594915b782af81dc5f80aa2e3a
SaeyoungChoi707 <63861341+SaeyoungChoi707@users.noreply.github.com>
2020-08-27 18:51:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# lololol
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ pAGE6.js
@@ -0,0 +1,16 @@
+//class kookie{
+   // constructor(){}
+    function kth(){
+      background(214, 130, 247);
+      if(state===5){
+      lolo = createInput("Browse Products");
+       fill("black");
+       
+       myg = createButton("SEARCH");
+       fill("black");
+        myg.position(100,160);
+        lolo.position(170,160);
+      }
+
+      
+   }
